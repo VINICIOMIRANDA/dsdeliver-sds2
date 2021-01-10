4d2702a40432f6838ec5b7fb42290344a2b3d576
vinicio.covalski <vinicio.covalski@gmail.com>
2021-01-10 11:11:20 -0300

    Alter Product List, Card, Types

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,12 +1,27 @@
 import './stysles.css';
-import React from "react";
-import StepsHeader from './StepsHeader'
+import React, { useEffect, useState } from "react";
+import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { Product } from './types';
+import { fetchProducts} from '../api';
 
 
 function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+    
+    useEffect(()=>{
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    }, []);
+
+
     return(
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products} />
         </div>
 
       
